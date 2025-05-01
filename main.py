17e43fcd1b7bc6cdda26c565c6f0467b410dfc88
Desperatiz <202360508+Desperatiz@users.noreply.github.com>
2025-05-01 10:53:26 -0600

    implemented the character_count function and displayed all characters with their respective count

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from stats import word_count
+from stats import character_count
 
 def main():
     book = get_book_text("books/frankenstein.txt")
     count = word_count(book)
+    char_count = character_count(book)
 
     print(f"{count} words found in the document")
+    for char in char_count:
+        print(f"'{char}': {char_count[char]}")
 
 def get_book_text(filepath):
     with open(filepath) as book:
--- stats.py
+++ stats.py
@@ -7,3 +7,15 @@ def word_count(book):
 
     word_count = len(words)
     return word_count
+
+def character_count(book):
+    characters = list(book.lower())
+    char_count = {}
+
+    for character in characters:
+        if character in char_count:
+            char_count[character] += 1
+        else:
+            char_count[character] = 1
+
+    return char_count
